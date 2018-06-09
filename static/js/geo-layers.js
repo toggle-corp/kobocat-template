7295def0815f2e15397f5606ce3c03987bca0314
Bibek Dahal <bibekdahal.bd16@gmail.com>
2018-06-09 21:49:50 +0545

    Add support for multiple geo layers

--- static/js/geo-layers.js
+++ static/js/geo-layers.js
@@ -1,5 +1,3 @@
-let activeGeoLayer;
-
 function getGeoJsonStyle() {
     return {
         fillOpacity: 0.2,
@@ -13,34 +11,72 @@ function initFeature(feature, layer) {
     return true;
 }
 
-function loadGeoLayer(geoLayerId) {
-    if (!geoLayerId) {
-        if (activeGeoLayer) {
-            map.removeLayer(activeGeoLayer);
-        }
-        return;
-    }
+const allLayers = {};
+let layerOrders = [];
 
-    $.getJSON('/fieldsight/geo-json/' + geoLayerId + '/', function(data) {
-        // TODO fix multiple async calls happening simultaneously
-
-        if (activeGeoLayer) {
-            map.removeLayer(activeGeoLayer);
-        }
-
-        activeGeoLayer = L.geoJson(data, {
-            style: getGeoJsonStyle,
-            onEachFeature: initFeature,
-        }).addTo(map);
-        activeGeoLayer.bringToBack();
-
-        map.flyToBounds(activeGeoLayer.getBounds(), { duration: 0.5 });
+function sortLayers() {
+    const reverse = [...layerOrders].reverse();
+    reverse.forEach((layerId) => {
+        allLayers[layerId] && allLayers[layerId].bringToBack();
     });
+
+    const firstLayer = allLayers[layerOrders[0]];
+    map.flyToBounds(firstLayer.getBounds(), { duration: 0.5 });
+}
+
+function loadGeoLayers(geoLayerIds) {
+    Object.keys(allLayers).forEach((layerId) => {
+        if (geoLayerIds.indexOf(layerId) === -1) {
+            map.removeLayer(allLayers[layerId]);
+            delete allLayers[layerId];
+        }
+    });
+
+    geoLayerIds.forEach((geoLayerId) => {
+        if (allLayers[geoLayerId]) {
+            return;
+        }
+
+        $.getJSON('/fieldsight/geo-json/' + geoLayerId + '/', function(data) {
+            if (layerOrders.indexOf(geoLayerId) === -1) {
+                return;
+            }
+
+            layer = L.geoJson(data, {
+                style: getGeoJsonStyle,
+                onEachFeature: initFeature,
+            }).addTo(map);
+
+            allLayers[geoLayerId] = layer;
+            sortLayers();
+        });
+    });
+
+    // if (!geoLayerId) {
+    //     if (activeGeoLayer) {
+    //         map.removeLayer(activeGeoLayer);
+    //     }
+    //     return;
+    // }
 }
 
 function initGeoLayers() {
     $('input[name="geo-layer"]').change(function() {
-        const geoLayerId = $(this).val();
-        loadGeoLayer(geoLayerId);
+        var selected = [];
+        layerOrders = [];
+
+        $('input[name="geo-layer"]').each(function() {
+            if ($(this).is(":checked")) {
+                selected.push($(this).val());
+                layerOrders.push({
+                    level: parseInt($(this).data('level'), 10),
+                    id: $(this).val(),
+                });
+            }
+        });
+        layerOrders.sort((a, b) => a.level - b.level);
+        layerOrders = layerOrders.map(l => l.id);
+
+        loadGeoLayers(selected);
     });
 }
